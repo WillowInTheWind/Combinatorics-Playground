39a84fa9f8ba79321a1abfee585549149d6579fb
WillowintheWind <wayland.chase@gmail.com>
2025-10-12 10:17:38 -0500

    proof of concept

--- src/app/bijection-playground/bijection-playground.component.ts
+++ src/app/bijection-playground/bijection-playground.component.ts
@@ -25,7 +25,6 @@ declare var LeaderLine: any;
   styleUrl: './bijection-playground.component.scss'
 })
 export class BijectionPlaygroundComponent {
-  selected = 'option2';
   N_MAX :number = 7;
   Current_N !: number;
   @ViewChild("leftcolumn") leftcenter!: ElementRef;
@@ -34,7 +33,7 @@ export class BijectionPlaygroundComponent {
   rowtwocards: CombinatorialObjectCard[] = [];
   rowonecards: CombinatorialObjectCard[] = [];
   class = "";
-  class_options = ["Fibbonacci", "Narayana", "Binomial Coefficent"]
+  class_options = ["Narayana", "Binomial Coefficent"]
   Set_options: string[] = [];
   Left_set_description = "";
   Right_set_description = "";
@@ -53,12 +52,9 @@ export class BijectionPlaygroundComponent {
     let cards = this.generate_cards_equaL_n(this.Current_N);
        this.rowonecards = cards[0]
        this.rowtwocards = cards[1]
-
-// console.log(this.rowonecards)
   }
    onNSelectChange(event:any) {
     this.Current_N = event
-
     console.log("N CHECKED", this.Current_N);
     let cards = this.generate_cards_equaL_n(this.Current_N);
     this.rowonecards = cards[0]
@@ -67,83 +63,49 @@ export class BijectionPlaygroundComponent {
   }
   onclassSelectChange(event:any) {
     this.class = event
-
-    switch (this.class) {
-      case "Fibbonacci":
-        this.Set_options = ["domino_tiling"]
-        // TO DO: DEAL WITH THIS ONE DIFFERENTLY
-        break
-      case "Narayana":
-        this.Set_options = ["fullparentheses", "dyckpaths"]
-        break
-      case "Binomial Coefficent":
-        this.Set_options = ["pixelstrips", "starfolkspaths"]
-
-        break
-      default:
-        this.Set_options = ["No class selected"]
-
-        break
-    }
+    switch (this.class) {case "Narayana":this.Set_options = ["fullparentheses", "dyckpaths"];break;
+      case "Binomial Coefficent":this.Set_options = ["pixelstrips", "starfolkspaths"];break;
+      default:this.Set_options = ["No class selected"];break;}
     this.class = event
-
   }
   onleftobjectSelectChange(event:any) {
     this.Left_set_description = event
     let cards = this.generate_cards_equaL_n(this.Current_N);
     this.rowonecards = cards[0]
     this.rowtwocards = cards[1]
-
-
   }
   onrightobjectSelectChange(event:any) {
     this.Right_set_description = event
     let cards = this.generate_cards_equaL_n(this.Current_N);
     this.rowonecards = cards[0]
     this.rowtwocards = cards[1]
-
   }
   cardSelected($event: any) {
-
     if (this.firstSelected == undefined) {
       this.firstSelected = $event;
+      return;
     }
-    else {
-      //TO-DO: FIX LOGIC TO CHECK FOR DUPLICATES
-      if ($event == 0 || $event[1].status % 10 == this.firstSelected.status % 10 || $event[1] == this.firstSelected[1]) {
-        console.log("Selected from same side")
-        return;
 
+      if ($event == 0 || $event[1].status % 10 == this.firstSelected.status % 10 || $event[1] == this.firstSelected[1]) {
+        return;
       }
       console.log(this.firstSelected[0].nativeElement);
-
-      // console.log(this.secondSelected.nativeElement);
-      // console.log(this.firstSelected.nativeElement.divColor)
       this.secondSelected = $event;
-      //console.log(this.secondSelected[0].nativeElement.ObjectCardComponent.returnData())
-      // this.firstSelected[0].nativeElement.objectInfo.status = 21;
       this.secondSelected[0].dragPosition = {x: 0 , y: 0};
-
-      // this.firstSelected.nativeElement.objectInfo.status = 12;
       let refrence = this.leftcenter.nativeElement.append(this.firstSelected[0].nativeElement)
       this.rightcenter.nativeElement.append(this.secondSelected[0].nativeElement);
       console.log(this.firstSelected[0].nativeElement.objectInfo)
       this.firstSelected = undefined;
       this.secondSelected = undefined;
-    }
+
   }
 
-//     }
      generate_cards_equaL_n(number: number) {
       let k_left = 0;
       let k_right = 0;
-
-      // for (let n = 0; n <= this.N_MAX; n++) {
-        let temp_list_one: any[] = []
-        let temp_list_two: any[] = []
-
+      let temp_list_one: any[] = [];
+      let temp_list_two: any[] = []
         for (let i = 0; i <= number; i++) {
-
           let row_one_images = this.cardRender.getCardsbyNandR(number, i, this.Left_set_description).subscribe((row_one_images) => {
               for (let image of row_one_images) {
                 temp_list_one.push({
@@ -157,15 +119,10 @@ export class BijectionPlaygroundComponent {
                 k_left++;
               }
             for (let i = temp_list_one.length - 1; i > 0; i--) {
-
-              // Generate Random Index
               const j = Math.floor(Math.random() * (i + 1));
-
-              // Swap elements
               [temp_list_one[i], temp_list_one[j]] = [temp_list_one[j], temp_list_one[i]];
             }
           });
-
           let row_two_images = this.cardRender.getCardsbyNandR(number, i, this.Right_set_description).subscribe((row_two_images) => {
               for (let image of row_two_images) {
                 temp_list_two.push({
@@ -174,24 +131,10 @@ export class BijectionPlaygroundComponent {
                 k_right++;
               }
             for (let i = temp_list_two.length - 1; i > 0; i--) {
-
-              // Generate Random Index
               const j = Math.floor(Math.random() * (i + 1));
-
-              // Swap elements
               [temp_list_two[i], temp_list_two[j]] = [temp_list_two[j], temp_list_two[i]];
             }
           });}
-
-
-
-
-        //
-        //
-        // }
-        // if (temp_list_one.length == 0) {
-        //   console.log("Card list was empty");
-        // }
         return [temp_list_one,temp_list_two];
 
     }
@@ -224,14 +167,7 @@ export class BijectionPlaygroundComponent {
     //   this.secondSelected = undefined;
     // }
   }
-  // rowonetracker(index: number, item: any): number {
-  //
-  //   return item.id;
-  // }
-  //
-  // rowtwotracker(index: number, item: any): number {
-  //   return item;
-  // }
+
 
   protected readonly Array = Array;
 }
